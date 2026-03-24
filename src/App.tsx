512444a483e31138729e6e82e85d7b70e4b3fded
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-03-24 15:27:44 +0300

    feat: implement search by title of product functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react'
 import ErrorMessage from './components/ErrorMessage'
 import Loader from './components/Loader'
 import ProductList from './components/ProductList'
@@ -8,19 +9,33 @@ import { useProducts } from './hooks/useProducts'
 
 export default function App() {
 
+  const [searchBar, setSearchBar] = useState<string>('')
+
   const { products, loading, error } = useProducts()
+
+  const searchedProduct = products.filter(product => (
+      product.title.toLocaleLowerCase().includes(searchBar.toLocaleLowerCase())
+    )
+  )
 
   return (
     <div className='mx-auto w-[90%] mt-[10vh]'>
       <h1 className='text-center text-xl mb-5'>
         Products
       </h1>
+      <input 
+        type="text"
+        className='outline-0 px-2 border mb-5 w-1/2'
+        placeholder='Search product...'
+        value={searchBar}
+        onChange={(e) => setSearchBar(e.target.value)} 
+      />
       { loading && <Loader /> }
       { error && <ErrorMessage error={error}/>}
 
       { products && 
         <ProductList 
-          products={products}
+          products={searchedProduct}
         />
       }
     </div>
