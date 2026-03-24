f922c8769affaaf8f438389a54e42f6b038a481e
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-03-24 12:52:11 +0300

    feat: install and implement framer-motion to add a smooth effect in list of product

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import type { IProduct } from '../types/product'
+import ProductCard from './ProductCard'
+
+interface IProductList {
+	products: IProduct[]
+}
+
+export default function ProductList({products}: IProductList) {
+	return (
+		<AnimatePresence mode="popLayout">
+			<div className='grid grid-cols-1 w-full sm:grid-cols-2 md:grid-cols-3'>
+				{products.map(product => (
+					<motion.li
+						key={product.id}
+						className='list-none'
+						layout
+						initial={{ opacity:0, y:10 }}
+						animate={{ opacity:1, y:0 }}
+						exit={{ opacity:0 }}
+						transition={{ duration:0.2 }}
+					>
+						<ProductCard 
+							product={product}
+						/>
+					</motion.li>
+				))}
+			</div>
+		</AnimatePresence>
+	)
+}
