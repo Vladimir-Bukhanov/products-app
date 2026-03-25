34fc64c3feebfa6fc9edda7343fd5b31a455b5f3
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-03-25 14:59:27 +0300

    feat: add local storage memory for products

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -4,18 +4,27 @@ import type { IProduct } from '../types/product'
 
 export const useProducts = () => {
 
-	 const [products, setProducts] = useState<IProduct[]>([])
+	const [products, setProducts] = useState<IProduct[]>([])
 
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<null | string>(null)
 
 	const fetchProducts = async () => {
+
+    const cashed = localStorage.getItem('products')
+
+    if (cashed) {
+      setProducts(JSON.parse(cashed))
+      return
+    }
+
       try {
         setError(null)
         setLoading(true)
         const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products')
         setProducts(response.data)
         setLoading(false)
+        localStorage.setItem('products', JSON.stringify(response.data))
       }
       catch (err: unknown) {
 				const error = err as AxiosError
