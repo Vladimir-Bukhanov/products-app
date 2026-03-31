086a35ef63f1dab1a1986182e77646fbd8ac883d
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-03-31 21:01:51 +0300

    feat: add smooth effect in cart items through framer-motion

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,6 +3,8 @@ import { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 import type { CartItem } from '../types/cartType'
 
+import { AnimatePresence, motion } from 'framer-motion'
+
 interface ICart {
 	cart: CartItem[]
 }
@@ -12,22 +14,34 @@ export default function Cart({ cart }: ICart) {
 	const { removeFromCart } = useContext(CartContext)
 
 	return (
-		<div className='mb-3'>
-			{cart.map(item => (
-				<div 
-					key={item.id}
-					className='mb-5 border p-3'
-				>
-					<p className='mb-3'>{item.title}</p>
-					<p className='mb-3'>Quantity: {item.quantity}</p>
-					<p className='mb-3'>Price: {item.price}$</p>
-					<button className='border cursor-pointer px-2 hover:bg-blue-100 duration-200'
-						onClick={() => removeFromCart(item.id)}
+		<AnimatePresence mode='popLayout'>
+			<div className='mb-3'>
+				{cart.map(item => (
+					<motion.li
+						key={item.title}
+						className='list-none'
+						layout
+						initial={{ opacity:0, y:10 }}
+						animate={{ opacity:1, y:0 }}
+						exit={{ opacity:0 }}
+						transition={{ duration:0.2 }}
 					>
-						Delete
-					</button>
-				</div>
-			))}
-		</div>
+						<div 
+							key={item.id}
+							className='mb-5 border p-3'
+						>
+							<p className='mb-3'>{item.title}</p>
+							<p className='mb-3'>Quantity: {item.quantity}</p>
+							<p className='mb-3'>Price: {item.price}$</p>
+							<button className='border cursor-pointer px-2 hover:bg-blue-100 duration-200'
+								onClick={() => removeFromCart(item.id)}
+							>
+								Delete
+							</button>
+						</div>
+					</motion.li>
+				))}
+			</div>
+		</AnimatePresence>
 	)
 }
